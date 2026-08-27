fc22b996983e6da5f1b714d157ee5a660806b2e9
Skibsky Ivan <skibsky.ivan@yandex.ru>
2026-08-27 15:29:21 +0300

    fix: use absolute paths for static assets in css and data  (Thu, Aug 27, 2026 03:29:21 PM)

--- src/data/audio-tracks.ts
+++ src/data/audio-tracks.ts
@@ -4,32 +4,32 @@ export const AUDIO_TRACKS: AudioTrack[] = [
   {
     id: 'Raise-Your-Mugs-High',
     name: 'Поднимайте кружки выше!',
-    path: './sounds/background-tracks/Raise-Your-Mugs-High.mp3',
+    path: '/sounds/background-tracks/Raise-Your-Mugs-High.mp3',
   },
   {
     id: 'Lively-Bard-Jig',
     name: 'Веселая джига барда',
-    path: './sounds/background-tracks/Lively-Bard-Jig.mp3',
+    path: '/sounds/background-tracks/Lively-Bard-Jig.mp3',
   },
   {
     id: 'The-Jolly-Tankard',
     name: 'Веселая кружка',
-    path: './sounds/background-tracks/The-Jolly-Tankard.mp3',
+    path: '/sounds/background-tracks/The-Jolly-Tankard.mp3',
   },
   {
     id: 'Medieval-Dance-Party',
     name: 'Средневековый танцевальный пир',
-    path: './sounds/background-tracks/Medieval-Dance-Party.mp3',
+    path: '/sounds/background-tracks/Medieval-Dance-Party.mp3',
   },
   {
     id: 'Pirate-Tavern-Riot',
     name: 'Бунт в пиратской таверне',
-    path: './sounds/background-tracks/Pirate-Tavern-Riot.mp3',
+    path: '/sounds/background-tracks/Pirate-Tavern-Riot.mp3',
   },
   {
     id: 'Tavern-Dance-Loop',
     name: 'Танец в таверне',
-    path: './sounds/background-tracks/Tavern-Dance-Loop.mp3',
+    path: '/sounds/background-tracks/Tavern-Dance-Loop.mp3',
   },
 ];
 
